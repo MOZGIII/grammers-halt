3c8c982669f7ec3e91a7002bc7b667aa65e1995c
MOZGIII <mike-n@narod.ru>
2024-10-24 16:57:14 -0300

    This alternative approach works

--- crates/bin/main/src/reconciler.rs
+++ crates/bin/main/src/reconciler.rs
@@ -9,24 +9,35 @@ pub struct Reconciler {
 
 impl Reconciler {
     pub async fn run(self) -> Result<core::convert::Infallible, crate::Error> {
-        let mut next_raw_update = Box::pin(self.client.next_raw_update());
-        let mut timer = Box::pin(tokio::time::sleep(std::time::Duration::from_secs(
-            1, /* first time wait less */
-        )));
+        let mut tasks = tokio::task::JoinSet::new();
 
-        loop {
-            tokio::select! {
-                result = &mut next_raw_update => {
-                    let (update, chats) = result?;
-                    self.handle_update(update, chats).await;
-                    next_raw_update = Box::pin(self.client.next_raw_update());
-                }
-                _ = &mut timer  => {
-                    self.reconcile().await?;
-                    timer = Box::pin(tokio::time::sleep(std::time::Duration::from_secs(30)));
+        let this = Arc::new(self);
+
+        tasks.spawn({
+            let this = Arc::clone(&this);
+            async move {
+                loop {
+                    let (update, chats) = this.client.next_raw_update().await.unwrap();
+                    this.handle_update(update, chats).await;
                 }
             }
-        }
+        });
+
+        tasks.spawn(async move {
+            tokio::time::sleep(std::time::Duration::from_secs(
+                1, /* first time wait less */
+            ))
+            .await;
+
+            loop {
+                this.reconcile().await.unwrap();
+                tokio::time::sleep(std::time::Duration::from_secs(30)).await;
+            }
+        });
+
+        let res = tasks.join_next().await.unwrap().unwrap();
+        tasks.shutdown().await;
+        res
     }
 
     async fn handle_update(
