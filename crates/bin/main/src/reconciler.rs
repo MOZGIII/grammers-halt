59f06976529748d375e43561be1bac06fe829826
MOZGIII <mike-n@narod.ru>
2024-10-25 19:57:32 -0300

    Don't use tasks

--- crates/bin/main/src/reconciler.rs
+++ crates/bin/main/src/reconciler.rs
@@ -9,11 +9,9 @@ pub struct Reconciler {
 
 impl Reconciler {
     pub async fn run(self) -> Result<core::convert::Infallible, crate::Error> {
-        let mut tasks = tokio::task::JoinSet::new();
-
         let this = Arc::new(self);
 
-        tasks.spawn({
+        let updates_progress = {
             let this = Arc::clone(&this);
             async move {
                 loop {
@@ -21,9 +19,9 @@ impl Reconciler {
                     this.handle_update(update, chats).await;
                 }
             }
-        });
+        };
 
-        tasks.spawn(async move {
+        let reconciler_progress = async move {
             tokio::time::sleep(std::time::Duration::from_secs(
                 1, /* first time wait less */
             ))
@@ -33,11 +31,10 @@ impl Reconciler {
                 this.reconcile().await.unwrap();
                 tokio::time::sleep(std::time::Duration::from_secs(30)).await;
             }
-        });
+        };
 
-        let res = tasks.join_next().await.unwrap().unwrap();
-        tasks.shutdown().await;
-        res
+        let (res1, _res2) = futures::future::join(updates_progress, reconciler_progress).await;
+        res1
     }
 
     async fn handle_update(
